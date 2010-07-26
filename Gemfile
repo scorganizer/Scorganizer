9aeb40ff825e266f3de9475842489c7c67c8e357
Matthew Boggan <scorganizer@googlegroups.com>
2010-07-25 19:03:35 -0700

    Added rspec

--- Gemfile
+++ Gemfile
@@ -3,5 +3,7 @@ source "http://rubygems.org"
 
 gem "nokogiri"
 gem "rails", "2.3.8"
-gem "rspec"
+gem "rspec", :require => "spec"
+gem "rspec-rails"
 gem "pg"
+
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -25,4 +25,9 @@ config.action_mailer.delivery_method = :test
 # Use SQL instead of Active Record's schema dumper when creating the test database.
 # This is necessary if your schema can't be completely dumped by the schema dumper,
 # like if you have constraints or database-specific column types
-# config.active_record.schema_format = :sql
+# config.active_record.schema_format = :sq
+
+#these lines were added for rspec config as told to by this page,
+# http://wiki.github.com/dchelimsky/rspec/configgem-for-rails
+config.gem "rspec", :lib => false, :version => ">= 1.2.0"
+config.gem "rspec-rails", :lib => false, :version => ">= 1.2.0"
